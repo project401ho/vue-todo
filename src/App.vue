68cac501408645814f6bfef8e10bc02a4f946ad7
YuSung Lee <yl8402@nyu.edu>
2021-03-30 10:04:42 +0900

    first day lecture
    Dev env set up
    component
    UI
    props / methods
    fontawsome

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="id">
+    <TodoHeader></TodoHeader> 
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    "TodoHeader": TodoHeader,
+    "TodoInput": TodoInput,
+    "TodoList": TodoList,
+    "TodoFooter":TodoFooter
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  body{
+    text-align: center;
+    background-color: #f1f1f1;
+  }
+  input{
+    border-style: groove;
+    width: 200px;
+
+  }
+  button{
+    border-style: groove;
+  }
+  .shadow{
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.3);
+  }
+
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="imputBox shadow">
+      <input type="text" v-model="newTodoItem">
+      <span class="addContainer">
+        <i class="fas fa-plus-circle addBtn" v-on:click="addTodo"></i>      
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods:{
+    addTodo:  function(){
+      localStorage.setItem(this.newTodoItem, this.newTodoItem)
+      this.clearInput();
+    },
+    clearInput:function(){
+      this.newTodoItem = "";
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+  .inputBox {
+    background: whitesmoke;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763Fb);
+    display:block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn{
+    color: whitesmoke;
+    vertical-align: middle;
+  }
+</style>
